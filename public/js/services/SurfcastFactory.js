f1aae04ccba9f1318dc15ddc7731e814d21f01c1
mbeezy <bolanos.mike@gmail.com>
2015-11-04 20:44:19 -0800

    Moved API call to the API project

--- public/js/services/SurfcastFactory.js
+++ public/js/services/SurfcastFactory.js
@@ -1,14 +1,14 @@
 angular.module('SurfcastFactory', []).factory('SurfcastFactory', ['$http', function($http) {
-  var urlBase = 'http://api.spitcast.com/api';
+  var urlBase = 'http://mb-me-api.azurewebsites.net/api';
   var factory = {};
 
   //http://api.spitcast.com/api/county/spots/san-diego/
   factory.getSdSurfSpots = function() {
-    return $http.get(urlBase + '/county/spots/san-diego/');
+    return $http.get(urlBase + '/surfspots');
   };
 
   factory.getSurfSpotForecast = function(id) {
-    return $http.get(urlBase + '/spot/forecast/' + id + '/');
+    return $http.get(urlBase + '/surfforecast/' + id);
   };
 
   return factory;
