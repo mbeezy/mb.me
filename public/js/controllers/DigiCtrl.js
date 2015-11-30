36dc72213272923c88ed7746fce439ec1077d75f
mbeezy <bolanos.mike@gmail.com>
2015-11-30 15:22:10 -0800

    Adding a default PHP session ID

--- public/js/controllers/DigiCtrl.js
+++ public/js/controllers/DigiCtrl.js
@@ -1,6 +1,7 @@
 angular.module('DigiCtrl', []).controller('DigiController', ['$scope', 'DigiFactory', function($scope, DigiFactory) {
 
     $scope.tagline = 'digiwaxx';
+    $scope.cookie = 'PHPSESSID=5f1c0d5e5967f7cc01fc0e9d31d40bff';
 
     $scope.initializeDownload = function(trackid, cookie) {
       console.log(trackid);
