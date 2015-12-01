f6fe05c417bfb1b37425c220d91a086dd6bbc6c1
mbeezy <bolanos.mike@gmail.com>
2015-11-30 19:12:50 -0800

    Added some error handling

--- public/js/controllers/DigiCtrl.js
+++ public/js/controllers/DigiCtrl.js
@@ -1,16 +1,19 @@
 angular.module('DigiCtrl', []).controller('DigiController', ['$scope', 'DigiFactory', function($scope, DigiFactory) {
 
     $scope.tagline = 'digiwaxx';
-    $scope.cookie = 'PHPSESSID=5f1c0d5e5967f7cc01fc0e9d31d40bff';
+    $scope.cookie = 'PHPSESSID=blahreplacemeblah';
 
     $scope.initializeDownload = function(trackid, cookie) {
       console.log(trackid);
       console.log(cookie);
 
-      DigiFactory.getMp3Link(trackid, cookie).success(function(data){
+      DigiFactory.getMp3Link(trackid, cookie)
+        .success(function(data){
+          $scope.mp3Links = data;
+        })
+        .error(function(data) {
           $scope.mp3Links = data;
         });
-
     };
 
 }]);
